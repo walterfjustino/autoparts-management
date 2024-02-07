1bc184ab9a968d4d86c7611d3e464a92926d426d
walterfjustino <walterfjustino@gmail.com>
2024-02-07 10:56:48 -0300

    [created structure of the project]

---
+++ src/main/kotlin/br/com/management/autoparts/AutoPartsApplication.kt
@@ -0,0 +1,11 @@
+package br.com.management.autoparts
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AutoPartsApplication
+
+fun main(args: Array<String>) {
+	runApplication<AutoPartsApplication>(*args)
+}
